dc8819c2bbfe1c1835e4e43065d47e387138b1be
nxb303 <nils.bauer@posteo.de>
2018-05-08 20:33:16 +0200

    Update downgrade_pdf.sh
    fixes

--- downgrade_pdf.sh
+++ downgrade_pdf.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#export PATH=/opt/local/bin:/opt/local/sbin:/Users/nils/bin:/usr/local/bin:/usr/local/sbin:/Users/nils/git/depot_tools:/Users/nils/anaconda/bin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 filename=$1
 backupSuffix=".bak"
@@ -7,6 +6,6 @@ backup=$filename$backupSuffix
 
 mv $filename $backup
 
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -o $filename $backup >> ../logs/downgrade_pdf_log.txt
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -o $filename $backup
 
-#rm $backup
+rm $backup
